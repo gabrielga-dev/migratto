6e152a10f39ed289014e5a28cd8dec2a555b2086
Gabriel <dealmeida.gabriel.d@gmail.com>
2025-10-24 17:16:45 -0300

    now, verifying files checksum

---
+++ model/migration/collection/migrationModelCollection.go
@@ -0,0 +1,20 @@
+package migration_collection_model
+
+import (
+	"fmt"
+
+	migration_model "github.org/gabrielga-dev/migratto/model/migration"
+)
+
+type MigrationModelCollection struct {
+	Migrations []migration_model.MigrationModel
+}
+
+func (m MigrationModelCollection) GetMigrationByTag(tag string) (migration_model.MigrationModel, error) {
+	for _, migration := range m.Migrations {
+		if migration.Tag == tag {
+			return migration, nil
+		}
+	}
+	return migration_model.MigrationModel{}, fmt.Errorf("migration with tag %s not found", tag)
+}
---
+++ model/migration/migrationModel.go
@@ -0,0 +1,22 @@
+package migration_model
+
+import (
+	"os"
+
+	file_service "github.org/gabrielga-dev/migratto/service/file"
+)
+
+type MigrationModel struct {
+	Filename string
+	Checksum string
+	Tag      string
+}
+
+func (m MigrationModel) IsEqual(file os.DirEntry, migrationsDir string) bool {
+	fileCheckSum, err := file_service.GetChecksum(migrationsDir + "/" + file.Name())
+	if err != nil {
+		return false
+	}
+
+	return m.Filename == file.Name() && m.Checksum == fileCheckSum
+}
